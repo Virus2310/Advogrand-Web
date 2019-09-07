672015c9c3747444f7a99c0c66ec40d262ac5331
Vlad <vladprodan6@gmail.com>
2019-09-07 17:36:38 +0300

    new blocks

--- js/main.js
+++ js/main.js
@@ -1,11 +1,41 @@
-$('.count-num').each(function () {
-      $(this).prop('Counter', 0).animate({
-            Counter: $(this).text()
-      }, {
-            duration: 4000,
-            easing: 'swing',
-            step: function (now) {
-                  $(this).text(Math.ceil(now));
-            }
-      });
+// $('.count-num').each(function () {
+//       $(this).prop('Counter', 0).animate({
+//             Counter: $(this).text()
+//       }, {
+//             duration: 4000,
+//             easing: 'swing',
+//             step: function (now) {
+//                   $(this).text(Math.ceil(now));
+//             }
+//       });
+// });
+
+let time = 2;
+let cc = 1;
+$(window).scroll(function () {
+   $('#counter').each(function () {
+      let
+         cPos = $(this).offset().top,
+         topWin = $(window).scrollTop();
+      if (cPos < topWin + 500) {
+         if (cc < 2) {
+            $('.count-num').addClass('viz');
+            $('span').each(function () {
+               let i = 1,
+                  num = $(this).data('num'),
+                  step = 1000 * time / num,
+                  that = $(this),
+                  int = setInterval(function () {
+                     if (i <= num) {
+                        that.html(i);
+                     } else {
+                        cc = cc + 2;
+                        clearInterval(int);
+                     }
+                     i++;
+                  }, step);
+            });
+         }
+      }
+   });
 });
