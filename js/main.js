9aac6488e701ace28a801d865e2b62dc6dad41f1
Vlad <vladprodan6@gmail.com>
2019-09-17 16:45:04 +0300

    add 2 new section, add owl-carousel

--- js/main.js
+++ js/main.js
@@ -1,14 +1,3 @@
-// $('.count-num').each(function () {
-//       $(this).prop('Counter', 0).animate({
-//             Counter: $(this).text()
-//       }, {
-//             duration: 4000,
-//             easing: 'swing',
-//             step: function (now) {
-//                   $(this).text(Math.ceil(now));
-//             }
-//       });
-// });
 
 let time = 2;
 let cc = 1;
@@ -39,3 +28,20 @@ $(window).scroll(function () {
       }
    });
 });
+
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
